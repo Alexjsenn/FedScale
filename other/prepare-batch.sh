291a6f13665b8e2f47fb8039611fbdd51fc7cd5e
gabriellemadden <gabriellemadden@savinetwork.ca>
2022-01-30 22:44:25 -0500

    Apply changes to batch scripts to fit environment

--- other/prepare-batch.sh
+++ other/prepare-batch.sh
@@ -5,10 +5,10 @@ SCREENNAME="prepare-new"
 screen -dmS $SCREENNAME
 
 
-for ii in {1..2}; do
+for ii in {101}; do
     WINDOW="agent"$ii
     echo $WINDOW
     screen -S $SCREENNAME -X screen -t $WINDOW
     screen -S $SCREENNAME -p $WINDOW -X stuff \
-    "~/FedScale/other/prepare-agents.sh agent$ii ^M"
+    "~/FedScale/other/prepare-agents.sh 10.30.74.$ii ^M"
 done
