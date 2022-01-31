9af0ec5cfebc6ab7b984aec6f20af44485b032e4
gabriellemadden <gabriellemadden@savinetwork.ca>
2022-01-30 22:44:25 -0500

    Apply changes to batch scripts to fit environment

--- other/fed-rsync.sh
+++ other/fed-rsync.sh
@@ -13,18 +13,21 @@ MAX=${args[MAX]}
 if [[ ${args[--core]} == 'true' ]]; then
     for ii in `seq $MIN $MAX`; do
         green "Copying core to agent$ii ..."
-        AGENT="agent${ii}"
+        AGENT="10.30.74.${ii}"
+
+        # rsync --rsync-path 'sudo -u root rsync' -ar \
+        #     ~/FedScale/core/argParser.py ubuntu@${AGENT}:~/FedScale/core/argParser.py &
         (rsync -ar \
             --exclude=*logs --exclude=*logging --exclude=*.pyc --exclude=*.pyx --exclude=__pycache__ --exclude=dataset \
-            --delete --info=progress2 ~/FedScale/ ${AGENT}:~/FedScale &)
-        (rsync -ar --info=progress2 --exclude=*.pyc --exclude=*.so --exclude=*.pyx --exclude=__pycache__ \
-             ~/anaconda3/envs/fedscale/lib/python3.6/site-packages/PIL/ \
-            ${AGENT}:~/anaconda3/envs/fedscale/lib/python3.6/site-packages/PIL &)
+            --delete --info=progress2 ~/FedScale/ ubuntu@${AGENT}:~/FedScale &)
+        # (rsync -ar --info=progress2 --exclude=*.pyc --exclude=*.so --exclude=*.pyx --exclude=__pycache__ \
+        #      ~/anaconda3/envs/fedscale/lib/python3.6/site-packages/PIL/ \
+        #     ${AGENT}:~/anaconda3/envs/fedscale/lib/python3.6/site-packages/PIL &)
     done
 elif [[ ${args[--data]} == 'true' ]]; then
     for ii in `seq $MIN $MAX`; do
         AGENT="agent${ii}"
-        green "Copying data to agent$ii ..."
+        green "Copying data to 10.30.74.$ii ..."
         (rsync -ar \
             --delete --info=progress2 ~/FedScale/dataset/data/FEMNIST ${AGENT}:~/FedScale/dataset/data/ &)
     done
