937826369e0c5f42d1cc5698a173697a584b23ce
Gabrielle Madden <gabriellemadden333@gmail.com>
2022-02-22 13:48:34 -0500

    Ensure cuda is disabled

--- core/argParser.py
+++ core/argParser.py
@@ -13,7 +13,7 @@ parser.add_argument('--num_executors', type=int, default=4)
 parser.add_argument('--executor_configs', type=str, default='')  # seperated by ;
 parser.add_argument('--total_worker', type=int, default=0)
 parser.add_argument('--data_map_file', type=str, default=None)
-parser.add_argument('--use_cuda', type=str, default='True')
+parser.add_argument('--use_cuda', type=str, default='False')
 parser.add_argument('--cuda_device', type=str, default=None)
 parser.add_argument('--base_port', type=int, default=51000)
 parser.add_argument('--time_stamp', type=str, default='logs')
